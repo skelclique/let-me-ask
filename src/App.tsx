d6702d0fc055866caa091509ce62420378bbea9b
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2021-12-29 19:23:54 -0300

    Create and use AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { NewRoom } from "./pages/NewRoom";
-import { Home } from "./pages/Home";
+import { AuthContextProvider } from './contexts/AuthContext';
+
+import { NewRoom } from './pages/NewRoom';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-room" element={<NewRoom />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/rooms/new" element={<NewRoom />} />
+        </Routes>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
