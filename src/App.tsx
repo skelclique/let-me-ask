43a3ce8a835ae964e0646cebb14a4adf86234f15
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2021-12-29 15:51:52 -0300

    Add new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/Home";
 
 function App() {
@@ -6,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/create-room" element={<NewRoom />} />
       </Routes>
     </BrowserRouter>
   );
