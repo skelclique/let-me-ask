96bca8ee028ad0c2cb20007a00224aec2d3250aa
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2022-01-04 16:20:39 -0300

    Page Home refactored

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,17 @@
 import { FormEvent, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { useAuth } from '../hooks/useAuth';
-import { Button } from '../components/Button';
+import { useAuth } from '../../hooks/useAuth';
+import { Button } from '../../components/Button';
 
 import { ref, get, child } from 'firebase/database';
-import { database } from '../services/firebase';
+import { database } from '../../services/firebase';
 
-import illustrationImg from '../assets/images/illustration.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
-import logoImg from '../assets/images/logo.svg';
+import illustrationImg from '../../assets/images/illustration.svg';
+import googleIconImg from '../../assets/images/google-icon.svg';
+import logoImg from '../../assets/images/logo.svg';
 
-import '../styles/auth.scss';
+import '../../styles/auth.scss';
 
 export function Home() {
   const navigate = useNavigate();
