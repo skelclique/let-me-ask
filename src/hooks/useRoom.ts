f47c01c7ebf6ec80551d5c5128d84ac854845157
Vinicius Bortolo <luizviniciusbortolo@gmail.com>
2022-01-05 20:30:39 -0300

    Create like functionality and stop onvalue event listener

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -1,6 +1,8 @@
-import { onValue, ref } from "firebase/database";
 import { useEffect, useState } from "react";
+
+import { onValue, ref } from "firebase/database";
 import { database } from "../services/firebase";
+import { useAuth } from "./useAuth";
 
 type FirebaseQuestions = Record<
   string,
@@ -12,6 +14,9 @@ type FirebaseQuestions = Record<
     content: string;
     isAnswered: boolean;
     isHighlighted: boolean;
+    likes: Record<string, {
+      authorId: string;
+    }>
   }
 >;
 
@@ -24,14 +29,17 @@ type QuestionType = {
   content: string;
   isAnswered: boolean;
   isHighlighted: boolean;
+  likeCount: number;
+  likeId: string | undefined;
 };
 
 export function useRoom(roomId: string | undefined) {
+  const { user } = useAuth();
   const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState('');
 
   useEffect(() => {
-    onValue(ref(database, `rooms/${roomId}`), (room) => {
+    const unsubscribe = onValue(ref(database, `rooms/${roomId}`), (room) => {
       const databaseRoom = room.val();
       const firebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {};
 
@@ -43,6 +51,8 @@ export function useRoom(roomId: string | undefined) {
             author: value.author,
             isHighlighted: value.isHighlighted,
             isAnswered: value.isAnswered,
+            likeCount: Object.values(value.likes ?? {}).length,
+            likeId: Object.entries(value.likes ?? {}).find(([key, like]) => like.authorId === user?.id)?.[0]
           };
         }
       );
@@ -50,7 +60,11 @@ export function useRoom(roomId: string | undefined) {
       setTitle(databaseRoom.title);
       setQuestions(parsedQuestions);
     });
-  }, [roomId]);
+
+    return () => {
+      unsubscribe();
+    }
+  }, [roomId, user?.id]);
 
   return { 
     questions, 
